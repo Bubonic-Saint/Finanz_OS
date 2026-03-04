920f118e42fbaa1225b56744622dfe99eae21f1d
Kevin <kevin.jaewoo.siess@gmail.com>
2026-03-04 12:24:11 +0100

    Initial commit: Projectstructure and First scripts

---
+++ scripts/extractor.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import json
+
+dateipfad = "data/ING_test.csv"
+regel_datei = "data/bank.json"
+
+with open(regel_datei, "r", encoding="utf-8") as f:
+    regel_datei = json.load(f)
+
