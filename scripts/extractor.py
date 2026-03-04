0e49b4a5cb96e5bc8783297c961e419a461d2f14
Kevin <kevin.jaewoo.siess@gmail.com>
2026-03-04 12:45:17 +0100

    add restructure

--- scripts/extractor.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import json
-
-dateipfad = "data/ING_test.csv"
-regel_datei = "data/bank.json"
-
-with open(regel_datei, "r", encoding="utf-8") as f:
-    regel_datei = json.load(f)
-
